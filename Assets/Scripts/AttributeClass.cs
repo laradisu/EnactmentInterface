2c1034ef5b6df1bbc8885e50ce9d603d17f6d244
rango3526 <r2chenore@gmail.com>
2020-05-19 00:45:11 -0400

    Add files via upload

--- Assets/Scripts/AttributeClass.cs
+++ Assets/Scripts/AttributeClass.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum AttributeType { Character, Object, Scene };
+
 public class AttributeClass : MonoBehaviour
 {
     public Sprite icon;
     public GameObject model;
-
+    public AttributeType attributeType = AttributeType.Character;
 }
