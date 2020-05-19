be620f0b07d115916aad500a112523bd0b8f18fa
Ranger Chenore <r2chenore@gmail.com>
2020-05-19 15:23:58 -0400

    Added scroll bar for Objects as well, and made characters and objects spawn when play button is pressed.

--- Assets/Scripts/Switch.cs
+++ Assets/Scripts/Switch.cs
@@ -1,17 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Switch : MonoBehaviour
 {
-    public GameObject canvas;
-    public void Activate() // opens the inital popup to character selection
-    {
-        canvas.SetActive(true);
+    public GameObject planningObj;
+    public GameObject timelineObj;
+    public GameObject enactmentObj;
 
-    }
-    public void playPressed() // closes the selection screens
+    public void SwitchToPlanningPhase() // opens the inital popup to character selection
     {
-        canvas.SetActive(false);
+        List<Tracker> allSpawnedObjs = FindObjectsOfType<Tracker>().ToList();
+        foreach (Tracker t in allSpawnedObjs)
+            Destroy(t.gameObject);
+
+        enactmentObj.SetActive(false);
+        planningObj.SetActive(true);
+    }
+
+    public void PlayPressed() // spawns first slide models then closes the selection screens
+    {
+        SwitchToEnactmentPhase();
+    }
+
+    public void StopPlayPressed() {
+        SwitchToPlanningPhase();
+    }
+
+    public void SwitchToEnactmentPhase() {
+        Transform currentSlide = timelineObj.transform.GetChild(0);
+        if (currentSlide == null)
+            return;
+
+        List<AttributeClass> allAttributes = currentSlide.GetComponentsInChildren<AttributeClass>().ToList();
+        foreach (AttributeClass ac in allAttributes) {
+            if (ac.model == null)
+                continue;
+            Instantiate(ac.model, GetRandomPositionNearZero(), Quaternion.identity);
+        }
+
+        planningObj.SetActive(false);
+        enactmentObj.SetActive(true);
+    }
+
+    Vector3 GetRandomPositionNearZero() {
+        return new Vector3(Random.Range(0, 10f) - 5f, Random.Range(0, 10f) - 5f, 0);
     }
 }
