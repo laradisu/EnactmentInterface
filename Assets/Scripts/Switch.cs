4966dfd66902d72b91d294e48d2b7ba10decd022
Ranger Chenore <r2chenore@gmail.com>
2020-06-05 15:45:51 -0400

    Added 3D models, changed camera to top-down angle, added scene navigation, bug fixes

--- Assets/Scripts/Switch.cs
+++ Assets/Scripts/Switch.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class Switch : MonoBehaviour
-{
+public class Switch : MonoBehaviour {
     public GameObject planningObj;
     public GameObject timelineObj;
     public GameObject enactmentObj;
+    int index = 0;
+
 
     public void SwitchToPlanningPhase() // opens the inital popup to character selection
     {
@@ -29,6 +30,7 @@ public class Switch : MonoBehaviour
     }
 
     public void SwitchToEnactmentPhase() {
+        index = 0;
         Transform currentSlide = timelineObj.transform.GetChild(0);
         if (currentSlide == null)
             return;
@@ -37,7 +39,40 @@ public class Switch : MonoBehaviour
         foreach (AttributeClass ac in allAttributes) {
             if (ac.model == null)
                 continue;
-            Instantiate(ac.model, GetRandomPositionNearZero(), Quaternion.identity);
+            Instantiate(ac.model, GetRandomPositionNearZero(), Quaternion.Euler(-90, 0, 0));
+        }
+
+        planningObj.SetActive(false);
+        enactmentObj.SetActive(true);
+
+    }
+    public void SwitchToEnactmentPhaseNext(bool backwards) {
+        int totalSlideCount = timelineObj.transform.childCount;
+
+        if (!backwards && index < totalSlideCount - 1)
+            index++;
+        else if (backwards && index > 0)
+            index--;
+        else {
+            SwitchToPlanningPhase();
+            return;
+        }
+
+        List<Tracker> allSpawnedObjs = FindObjectsOfType<Tracker>().ToList();
+        foreach (Tracker t in allSpawnedObjs)
+            Destroy(t.gameObject);
+
+        Transform currentSlide = timelineObj.transform.GetChild(index);
+
+        if (currentSlide == null)
+            return;
+
+        List<AttributeClass> allAttributes = currentSlide.GetComponentsInChildren<AttributeClass>().ToList();
+
+        foreach (AttributeClass ac in allAttributes) {
+            if (ac.model == null)
+                continue;
+            Instantiate(ac.model, GetRandomPositionNearZero(), Quaternion.Euler(-90, 0, 0));
         }
 
         planningObj.SetActive(false);
