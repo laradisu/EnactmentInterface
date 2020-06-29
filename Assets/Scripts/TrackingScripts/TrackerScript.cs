dc6dc406ffe56f5fad9fce98b7a9a8ad289576ff
ELX-Lab <r2chenore@gmail.com>
2020-06-28 21:59:32 -0400

    Modified some of the YOLO tracking scripts

--- Assets/Scripts/TrackingScripts/TrackerScript.cs
+++ Assets/Scripts/TrackingScripts/TrackerScript.cs
@@ -20,22 +20,11 @@ public class TrackerScript : MonoBehaviour {
 
     public float xMotionDamping = 1f;
     public float yMotionDamping = 1f;
-    public float zMotionDamping = 0.2f;
-
-    public float zOffset = 2f;
 
     // range of the character/object in camera coordinates
     public Vector2 xRange = new Vector2(-50, 50);
     public Vector2 yRange = new Vector2(0, 75);
-    public Vector2 zRange = new Vector2(0, 30);
     public bool useRangeNotDamping = true;
-
-    public bool keyboardInputDepth = true;
-    public bool keyboardInputYRot = true;
-    public bool useShiftInput = false;
-
-    public bool mapToPhoneRotation = false;
-    public bool turnAroundY = false;
 
     public Vector3 targetPosition;
 
@@ -61,21 +50,11 @@ public class TrackerScript : MonoBehaviour {
     // Message to be sent
     //string strMessageSend = "";
 
-    // info strings, no need to change to float/double
-    public string stringX = "";
-    public string stringY = "";
-    public string stringZ = "";
-    public string stringYaw = "";
-    public string stringP = "";
-    public string stringR = "";
 
-
-    public double valueX = 0;
-    public double valueY = 0;
-    public double valueZ = 0;
-    public double valueYaw = 0;
-    public double valueP = 0;
-    public double valueR = 0;
+    double valueX = 0;
+    double valueY = 0;
+    double width = 0;
+    double height = 0;
 
     bool shouldChangePos = false;
 
@@ -83,7 +62,6 @@ public class TrackerScript : MonoBehaviour {
     void Start() {
         valueX = transform.position.x;
         valueY = transform.position.y;
-        valueZ = transform.position.z;
 
         targetPosition = transform.position;
 
@@ -99,23 +77,14 @@ public class TrackerScript : MonoBehaviour {
     //originally no update function at all, all stuff in onGUI()
     void FixedUpdate() {
         var pos = this.transform.position;
-        Quaternion rot = this.transform.rotation;
 
+	// set positions to bottom center of YOLO tracking box
+	valueX += width/2;
+	valueY += height;
 
-        if (keyboardInputDepth) {
-            if (useShiftInput == Input.GetKey(KeyCode.LeftShift)) {
-                if (Input.GetKey(KeyCode.End))
-                    zOffset -= 0.2f;
-                if (Input.GetKey(KeyCode.Home))
-                    zOffset += 0.2f;
-            }
-        }
-
-        pos.z = zOffset + 0f * (float)valueZ * zMotionDamping;  // Z IS CONSTANT NOW (with keyboard input)
         if (useRangeNotDamping) {
             float xRatio = (float)valueX / (float)xRange.y;
             float yRatio = (float)valueY / (float)yRange.y;
-            float zRatio = (float)valueZ / (float)zRange.y;
 
             pos.x = xRatio * xGameRange.y;
             pos.y = yRatio * yGameRange.y;
@@ -123,42 +92,6 @@ public class TrackerScript : MonoBehaviour {
         else {
             pos.x = (float)valueX * xMotionDamping;
             pos.y = (float)valueY * yMotionDamping;
-        }
-
-        /*
-            rot.x = (float) valueYaw;
-            rot.y = (float) valueP;
-            rot.z = (float) valueR;
-        */
-
-        if (mapToPhoneRotation) {
-            valueR = -valueR;
-            var tempyaw = valueYaw;
-            valueYaw = -valueP;
-            valueP = -tempyaw;
-        }
-
-        if (turnAroundY) {
-            valueP += 180;
-        }
-
-        if (keyboardInputYRot) {
-            if (useShiftInput == Input.GetKey(KeyCode.LeftShift)) {
-                if (Input.GetKey(KeyCode.LeftArrow))
-                    valueP += 6;
-                if (Input.GetKey(KeyCode.RightArrow))
-                    valueP -= 6;
-
-                if (Input.GetKey(KeyCode.UpArrow))
-                    valueYaw += 6;
-                if (Input.GetKey(KeyCode.DownArrow))
-                    valueYaw -= 6;
-            }
-        }
-
-        if (Input.GetKey(KeyCode.LeftControl) && Input.GetKey(KeyCode.T)) {
-            valueYaw = 0;
-            valueR = 0;
         }
 
         if (flipX)
@@ -176,8 +109,6 @@ public class TrackerScript : MonoBehaviour {
             targetPosition = pos;
             shouldChangePos = false;
         }
-        if (!usingYolo)
-            this.transform.rotation = Quaternion.Euler((float)valueYaw, (float)valueP, (float)valueR);
 
         if (Vector3.Magnitude(targetPosition - transform.position) > 0.05f) {
             if (!smoothMovements)
@@ -222,32 +153,12 @@ public class TrackerScript : MonoBehaviour {
                 IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
                 byte[] data = client.Receive(ref anyIP);
 
-                if (!usingYolo) {
-                    //choose one, double here
+                if (usingYolo) {
                     valueX = BitConverter.ToDouble(data, 0);
-                    stringX = valueX.ToString();
                     valueY = BitConverter.ToDouble(data, 8);
-                    stringY = valueY.ToString();
-                    valueZ = BitConverter.ToDouble(data, 16);
-                    stringZ = valueZ.ToString();
-
-
-                    valueYaw = BitConverter.ToDouble(data, 24);
-                    stringYaw = valueYaw.ToString();
-                    valueP = BitConverter.ToDouble(data, 32);
-                    stringP = valueP.ToString();
-                    valueR = BitConverter.ToDouble(data, 40);
-                    stringR = valueR.ToString();
-                }
-                else {
-                    valueX = BitConverter.ToDouble(data, 0);
-                    stringX = valueX.ToString();
-                    valueY = BitConverter.ToDouble(data, 8);
-                    stringY = valueY.ToString();
-                    valueZ = 500 / (BitConverter.ToDouble(data, 16));
-                    stringZ = valueZ.ToString();
+                    width = BitConverter.ToDouble(data, 16);
+		    height = BitConverter.ToDouble(data, 24);
                     shouldChangePos = true;
-                    Debug.Log("Shoudl change pos");
                 }
             }
             catch (Exception err) {
